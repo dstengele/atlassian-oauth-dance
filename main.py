c681498b65821479e179f84f2cb754d58980d830
Dennis Stengele <dennis@stengele.me>
2021-07-30 12:50:24 +0200

    Automatically generate RSA keys if none is provided

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 from urllib.parse import parse_qsl
 import requests
 from requests import HTTPError
@@ -7,6 +9,7 @@ from requests_oauthlib import OAuth1
 from oauthlib.oauth1 import SIGNATURE_RSA, SIGNATURE_TYPE_QUERY
 import webbrowser
 import click
+from Crypto.PublicKey import RSA
 
 
 @click.command()
@@ -28,15 +31,26 @@ import click
 )
 @click.option(
     "--rsa-private-key",
-    type=click.File("r"),
-    prompt="RSA Private Key File",
+    type=click.Path(resolve_path=True, dir_okay=False),
     help="Path to the RSA Private Key file to use for OAuth",
+    default="private.pem",
 )
 def main(base_url, app_type, consumer_key, rsa_private_key):
     request_token_url = f"{base_url}/plugins/servlet/oauth/request-token"
     base_authorization_url = f"{base_url}/plugins/servlet/oauth/authorize"
     access_token_url = f"{base_url}/plugins/servlet/oauth/access-token"
-    rsa_key = rsa_private_key.read()
+
+    # Check if private key file is valid and generate new one if neccessary
+    try:
+        with open(rsa_private_key, "r") as rsa_private_key_file:
+            rsa_key_string = rsa_private_key_file.read()
+            rsa_key = RSA.import_key(rsa_key_string)
+    except (ValueError, FileNotFoundError):
+        print("Key missing or invalid, generating new...")
+        with open(rsa_private_key, "wb") as rsa_private_key_file, open("public.pem", "wb") as rsa_public_key_file:
+            rsa_key = RSA.generate(2048)
+            rsa_private_key_file.write(rsa_key.export_key())
+            rsa_public_key_file.write(rsa_key.publickey().export_key())
 
     # Get request token
     oauth = OAuth1(
@@ -47,7 +61,11 @@ def main(base_url, app_type, consumer_key, rsa_private_key):
         callback_uri="",
     )
     r = requests.post(request_token_url, verify=True, auth=oauth)
-    r.raise_for_status()
+    try:
+        r.raise_for_status()
+    except HTTPError:
+        print(f"Error while getting request token. Error was: {r.text}")
+        sys.exit(1)
     request = dict(parse_qsl(r.text))
     request_token = request["oauth_token"]
     request_token_secret = request["oauth_token_secret"]
@@ -66,7 +84,11 @@ def main(base_url, app_type, consumer_key, rsa_private_key):
         resource_owner_secret=request_token_secret,
     )
     r = requests.post(access_token_url, verify=True, auth=oauth)
-    r.raise_for_status()
+    try:
+        r.raise_for_status()
+    except HTTPError:
+        print(f"Error while getting access token. Error was: {r.text}")
+        sys.exit(1)
     access = dict(parse_qsl(r.text))
     access_token = access.get("oauth_token")
     access_token_secret = access.get("oauth_token_secret")
@@ -86,6 +108,7 @@ def main(base_url, app_type, consumer_key, rsa_private_key):
         r.raise_for_status()
     except HTTPError:
         print("Error when trying to use the new OAuth credentials!")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
